b27fecf252fa8faf71d9c56f9953fc721fcca647
paunicole <nicolecardozogomez@gmail.com>
2023-03-30 23:31:57 -0300

    creo gui sala

---
+++ cinemar/GUI/gui_sala.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+
+class Sala(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title("Sala")
+        self.geometry("960x600")
+        self.resizable(0, 0)
+        self.columnconfigure(1, weight=2)
+        
+        #self.img = tk.PhotoImage(file = r"GUI/source/img/cuadrado.png")
+        #self.img2 = tk.PhotoImage(file = r"GUI/source/img/cuadrado_relleno.png")
+        #self.img_pantalla = tk.PhotoImage(file = r"GUI/source/img/pantalla.png")
+        
+        self.show()
+
+    def show(self):
+
+        #butaca = tk.Label(self.entrada_frame, image=self.img_pantalla)
+        #butaca.grid(row=0, column=0, ipady=10)
+
+        self.frame = tk.Frame(self, width=300, height=350)
+        self.frame.grid(row=0, column=0)
+
+        filas = 5
+        columnas = 9
+        cont = 1
+        letras = 'ABCDEFGHIJKLMN'
+
+        for i in range(filas):
+            letra = tk.Label(self.frame, text=letras[i], font='Arial')
+            letra.grid(row=i, column=0)
+            
+            for j in range(1, columnas+1):
+                butaca = tk.Checkbutton(self.frame,
+                                    text=f"{cont}",
+                                    bd=3,
+                                    font='Arial',
+                                    width=3,
+                                    selectcolor='yellow',
+                                    indicatoron=False)
+                butaca.grid(row=i, column=j, padx=5, pady=5)
+                cont += 1
+            letra = tk.Label(self.frame, text=letras[i], font='Arial')
+            letra.grid(row=i, column=j+1)
+
+
+app = Sala()
+app.mainloop()
